421f42be20e179bd30d41f3a25bcd573cbc749c6
Vlad Kugot <vladkygot@gmail.com>
2026-06-15 15:24:35 +0300

    Add Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,25 @@
+import "./Footer.scss";
 
 export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <div className="footer__content">
+          <div className="footer__img-logo">FASCO</div>
 
-    return (
-        <footer>
-            Footer
-        </footer>
-    )
-}
+          <ul className="footer__nav">
+            <li className="footer__item nav__item-link">Support Center</li>
+            <li className="footer__item nav__item-link">Invaicing</li>
+            <li className="footer__item nav__item-link">Contract</li>
+            <li className="footer__item nav__item-link">Careers</li>
+            <li className="footer__item nav__item-link">Blog</li>
+            <li className="footer__item nav__item-link">FAQ,s</li>
+          </ul>
+        </div>
+        <p className="footer__copyright">
+          Copyright © 2026 Xpro . All Rights Reseved.
+        </p>
+      </div>
+    </footer>
+  );
+};
